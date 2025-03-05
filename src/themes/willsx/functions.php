d9d4f5f6797a010c407d8c003dde02f18edc8eb7
richardfarnhill <richardfarnhill@yahoo.com>
2025-03-05 06:47:27 +0000

    Fix(theme): add missing theme-functions.php include

--- src/themes/willsx/functions.php
+++ src/themes/willsx/functions.php
@@ -236,6 +236,11 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Theme-specific functions.
+ */
+require get_template_directory() . '/inc/theme-functions.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
